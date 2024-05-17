64318be9d441150bb6de81ab12bf002415ff6f3a
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-16 21:53:38 -0300

    feat: adding productVO

---
+++ Services/GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.ProductAPI.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace GeekShopping.ProductAPI.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImgURL { get; set; }
+    }
+}
