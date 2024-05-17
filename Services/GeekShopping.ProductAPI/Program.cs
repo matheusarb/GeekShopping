3a050f9d5cbcb03d3a1552d4892b75f520e0514d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-16 21:54:16 -0300

    feat: adding services on program.cs

--- Services/GeekShopping.ProductAPI/Program.cs
+++ Services/GeekShopping.ProductAPI/Program.cs
@@ -1,5 +1,9 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Models.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "1.0.0" });
+});
 builder.Services.AddDbContext<MySQLContext>(options =>
 {
     var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
@@ -17,6 +24,11 @@ builder.Services.AddDbContext<MySQLContext>(options =>
         new MySqlServerVersion(new Version(8, 0, 36))
         );
 });
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
