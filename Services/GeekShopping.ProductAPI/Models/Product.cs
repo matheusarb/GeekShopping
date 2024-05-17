b8e29c4dbd219f4de331ac75b8c39449274fc07d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-16 21:53:07 -0300

    feat: class mapper and models

--- Services/GeekShopping.ProductAPI/Models/Product.cs
+++ Services/GeekShopping.ProductAPI/Models/Product.cs
@@ -1,6 +1,9 @@
-﻿using GeekShopping.ProductAPI.Models.Base;
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Models.Base;
+using Microsoft.IdentityModel.Tokens;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace GeekShopping.ProductAPI.Models
 {
@@ -23,10 +26,19 @@ namespace GeekShopping.ProductAPI.Models
 
         [Column("category_name")]
         [StringLength(50)]
-        public string CategoryName { get; set; }
+        public string? CategoryName { get; set; }
 
         [Column("img_url")]
         [StringLength(300)]
-        public string ImgURL { get; set; }
+        public string? ImgURL { get; set; }
+
+        public void Update(ProductVO entity)
+        {
+            Name = entity.Name.IsNullOrEmpty() ? Name : entity.Name;
+            Price = entity.Price <= 0 ? Price : entity.Price;
+            Description = entity.Description.IsNullOrEmpty() ? Description : entity.Description;
+            CategoryName = entity.CategoryName.IsNullOrEmpty() ? CategoryName : entity.CategoryName;
+            ImgURL = entity.ImgURL.IsNullOrEmpty() ? ImgURL : entity.ImgURL;
+        }
     }
 }
